fe9dbce925407c278b0a738f4ea79097118bfa41
Thanys Bernabé <t.bernabe@peritus.es>
2023-10-17 19:45:49 +0200

    Final style

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-gradient-to-tr from-[#762BC6] to-[#C751E6] p-80">
-      <section className="flex flex-col items-center justify-center bg-[#0A0B1B] rounded-xl w-full p-8">
+    <main className="flex h-screen flex-col items-center justify-center bg-gradient-to-tr from-[#762BC6] to-[#C751E6] p-8">
+      <section className="flex flex-col items-center justify-center bg-[#0A0B1B] rounded-xl w-full md:w-auto p-8">
         <h1 className="bg-gradient-to-r from-[#762BC6] to-[#C751E6] inline-block text-transparent bg-clip-text text-2xl font-bold">
           SerialQuizz
         </h1>
-        <div className="my-8 p-8 bg-[#4A5567] w-full rounded-lg">
-          <div className="text-[#96A2B7] text-center text-5xl leading-6 h-4 tracking-wider">
+        <div className="my-8 py-6 md:px-32 bg-[#4A5567] w-full rounded-lg">
+          <div className="text-[#96A2B7] text-center text-3xl md:text-5xl tracking-wider">
             <span>l m m j v s</span>
           </div>
         </div>
-        <div className="flex items-center justify-between text-[#4A5567] w-full px-10">
+        <div className="flex items-center justify-evenly flex-col md:flex-row text-[#4A5567] w-full">
           <div>
             <span>Intentos: </span>
             <span className="text-[#96A2B7]">4/5</span>
@@ -19,6 +19,23 @@ export default function Home() {
             <span>Errores: </span>
             <span className="text-[#96A2B7]">a, b, c</span>
           </div>
+        </div>
+        <div className="flex items-center justify-center my-8 md:my-12">
+          <input
+            type="text"
+            autoFocus={true}
+            maxLength={1}
+            required
+            className="w-24 p-4 text-center bg-transparent border-[#4A5567] border-4 outline-none text-white text-5xl rounded-lg lowercase focus:border-[#C950E7]"
+          />
+        </div>
+        <div className="flex items-center justify-around flex-col-reverse gap-y-4 w-full md:flex-row">
+          <button className="bg-[#C950E7] text-white px-8 py-2 rounded-lg text-lg border-b-[#7B258A] border-b-4 transition ease-in-out hover:bg-[#de5dff] duration-300 active:bg-[#ebb3f9]">
+            Nueva serie
+          </button>
+          <button className="bg-[#C950E7] text-white px-8 py-2 rounded-lg text-lg border-b-[#7B258A] border-b-4 transition ease-in-out hover:bg-[#de5dff] duration-300 active:bg-[#ebb3f9]">
+            Resolver
+          </button>
         </div>
       </section>
     </main>
