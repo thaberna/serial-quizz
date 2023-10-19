45bd51810aade82adcde2ddcef1f54154cbf59b0
Thanys Bernabé <t.bernabe@peritus.es>
2023-10-19 19:32:38 +0200

    Logic game

--- app/page.js
+++ app/page.js
@@ -1,27 +1,71 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import serials from "./constants/serials";
+import { useEffect, useRef, useState } from "react";
+import sequences from "./constants/sequences";
 
 export default function Home() {
-  const [sequence, setSequence] = useState("");
+  const [sequence, setSequence] = useState("c a r g a n d");
+  const [solution, setSolution] = useState("");
+  const [serial, setSerial] = useState("");
+  const serialRef = useRef();
+  const [tries, setTries] = useState(0);
+  const [mistakes, setMistakes] = useState();
 
   const getSequence = () => {
-    const uncheckedSerials = serials.filter((serial) => !serial.checked);
+    const uncheckedSerials = sequences.filter((sequence) => !sequence.checked);
 
     if (uncheckedSerials.length > 0) {
       const randomIndex = Math.floor(Math.random() * uncheckedSerials.length);
       const randomSequence = uncheckedSerials[randomIndex];
-      console.log(randomSequence);
       setSequence(randomSequence.content);
+      setSolution(randomSequence.solution);
     } else {
       console.log("Todas las series han sido resueltas");
     }
   };
 
-  useEffect(() => {
+  const handleNewSeries = () => {
     getSequence();
-  }, [sequence]);
+    setSerial("");
+    serialRef.current.focus();
+    setTries(0);
+    setMistakes();
+  };
+
+  const handleSolve = () => {
+    if (serial === "") {
+      alert("¿Te has quedado en blanco?");
+      serialRef.current.focus();
+      return;
+    }
+
+    if (solution !== serial) {
+      // Tries & Mistakes
+      if (tries <= 5) {
+        setTries((prevNumber) => prevNumber + 1);
+        setMistakes((prevMistake) =>
+          prevMistake
+            ? prevMistake + ", " + serial.toString()
+            : serial.toString()
+        );
+        setSerial("");
+        serialRef.current.focus();
+      } else {
+        alert("Te has quedado sin intentos. Mejor prueba con otra serie...");
+        handleNewSeries();
+      }
+    }
+
+    // If correct, save in localStorage the checked value
+
+    // Show another sequence
+    alert("Ou yeah! ¿Probamos con otra?");
+    handleNewSeries();
+  };
+
+  useEffect(() => {
+    handleNewSeries();
+  }, []);
 
   return (
     <main className="flex h-screen flex-col items-center justify-center bg-gradient-to-tr from-[#762BC6] to-[#C751E6] p-8">
@@ -37,27 +81,35 @@ export default function Home() {
         <div className="flex items-center justify-evenly flex-col md:flex-row text-[#4A5567] w-full">
           <div>
             <span>Intentos: </span>
-            <span className="text-[#96A2B7]">4/5</span>
+            <span className="text-[#96A2B7]">{tries}/5</span>
           </div>
           <div>
             <span>Errores: </span>
-            <span className="text-[#96A2B7]">a, b, c</span>
+            <span className="text-[#96A2B7]">{mistakes}</span>
           </div>
         </div>
         <div className="flex items-center justify-center my-8 md:my-12">
           <input
             type="text"
-            autoFocus={true}
             maxLength={1}
             required
+            ref={serialRef}
+            value={serial}
+            onChange={(e) => setSerial(e.target.value)}
             className="w-24 p-4 text-center bg-transparent border-[#4A5567] border-4 outline-none text-white text-5xl rounded-lg lowercase focus:border-[#C950E7]"
           />
         </div>
         <div className="flex items-center justify-around flex-col-reverse gap-y-4 w-full md:flex-row">
-          <button className="bg-[#C950E7] text-white px-8 py-2 rounded-lg text-lg border-b-[#7B258A] border-b-4 transition ease-in-out hover:bg-[#de5dff] duration-300 active:bg-[#ebb3f9]">
+          <button
+            onClick={handleNewSeries}
+            className="bg-[#C950E7] text-white px-8 py-2 rounded-lg text-lg border-b-[#7B258A] border-b-4 transition ease-in-out hover:bg-[#de5dff] duration-300 active:bg-[#ebb3f9]"
+          >
             Nueva serie
           </button>
-          <button className="bg-[#C950E7] text-white px-8 py-2 rounded-lg text-lg border-b-[#7B258A] border-b-4 transition ease-in-out hover:bg-[#de5dff] duration-300 active:bg-[#ebb3f9]">
+          <button
+            onClick={handleSolve}
+            className="bg-[#C950E7] text-white px-8 py-2 rounded-lg text-lg border-b-[#7B258A] border-b-4 transition ease-in-out hover:bg-[#de5dff] duration-300 active:bg-[#ebb3f9]"
+          >
             Resolver
           </button>
         </div>
